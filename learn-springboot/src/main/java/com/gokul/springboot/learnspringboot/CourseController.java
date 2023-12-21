f8de88c6eaefd11f127022cf9e40335e3224355d
Gokul-sv <gokulksp@gmail.com>
2023-12-21 12:42:02 +0530

    Adding spring-boot repo

---
+++ learn-springboot/src/main/java/com/gokul/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,17 @@
+package com.gokul.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(
+				new Course(1, "Learn SpringBoot", "Gokul"),
+				new Course(2, "Learn LLD", "Gokul"));
+	}
+}
